fc2fb480e25672b6b1b902b0b0b58db151bd4cae
Bickrom Shorma <bickrombickrom21@gmail.com>
2026-08-10 20:53:03 +0600

    Time: 186 ms (5.83%), Space: 24.2 MB (92.87%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+      for (int i = 0; i < names.size() - 1; i++) {
+       for (int j = 0; j < names.size() - i - 1; j++) {
+
+        if (heights[j] < heights[j + 1]) {
+            swap(heights[j], heights[j + 1]);
+            swap(names[j], names[j + 1]);
+        }
+    }
+}
+    return names;
+    }
+};
